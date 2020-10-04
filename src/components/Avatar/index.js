5313cff849c70ce4b774ba6e534ba890d3d08d18
Rodolfo Marriel <rodolfo.29r@gmail.com>
2020-10-04 15:23:10 -0300

    Configurando carregamento de imagem

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Image from '../../images/profile-photo.jpg'
 
+import LazyLoad from 'react-lazy-load';
+
 import * as S from './styled'
 
 const Avatar = () => {
 
-  return <S.AvatarWrapper src={Image}/>
+  return (
+    <LazyLoad debounce={false} offsetVertical={500}>
+      <S.AvatarWrapper src={Image} />
+    </LazyLoad>
+  )
 }
 
 export default Avatar
